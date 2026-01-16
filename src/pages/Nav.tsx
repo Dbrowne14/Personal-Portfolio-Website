92bf9abf47900237725789b3c9f3ca9cecd0c127
Dbrowne14 <davidbrowne1992@gmail.com>
2026-01-16 17:07:12 +0000

    added transitions

--- src/pages/Nav.tsx
+++ src/pages/Nav.tsx
@@ -6,16 +6,16 @@ const Nav = () => {
     <div className="w-full">
       <ul className="flex flex-row gap-[min(8vw,8rem)] w-full mb-5 pb-2 text-[min(5vw,1.3rem)]">
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/" className="nav-link">home</NavLink>
         </li>
         <li>
-          <NavLink to="about">About</NavLink>
+          <NavLink to="about" className="nav-link">about</NavLink>
         </li>
         <li>
-          <NavLink to="/projects"> Projects </NavLink>
+          <NavLink to="/projects" className="nav-link"> projects </NavLink>
         </li>
         <li>
-          <NavLink to="/contact"> Contact </NavLink>
+          <NavLink to="/contact" className="nav-link"> contact </NavLink>
         </li>
       </ul>
     </div>
