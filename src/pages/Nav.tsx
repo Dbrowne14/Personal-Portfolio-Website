e1e835e95a6258efcf9b6e0bea6e705355158449
Dbrowne14 <davidbrowne1992@gmail.com>
2026-01-16 20:36:21 +0000

    changes to nav

--- src/pages/Nav.tsx
+++ src/pages/Nav.tsx
@@ -3,19 +3,41 @@ import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div className="w-full">
-      <ul className="flex flex-row gap-[min(8vw,8rem)] w-full mb-5 pb-2 text-[min(5vw,1.3rem)]">
+    <div className="w-full mt-4 sticky">
+      <ul className="flex flex-row gap-[clamp(1rem,4vw,6rem)] justify-center w-full mb-5 pb-2 text-[min(10vw,1.3rem)]">
         <li>
-          <NavLink to="/" className="nav-link">home</NavLink>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "selected" : "nav-link")}
+          >
+            home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="about" className="nav-link">about</NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? "selected" : "nav-link")}
+          >
+            about
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/projects" className="nav-link"> projects </NavLink>
+          <NavLink
+            to="/projects"
+            className={({ isActive }) => (isActive ? "selected" : "nav-link")}
+          >
+            {" "}
+            projects{" "}
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/contact" className="nav-link"> contact </NavLink>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) => (isActive ? "selected" : "nav-link")}
+          >
+            {" "}
+            contact{" "}
+          </NavLink>
         </li>
       </ul>
     </div>
