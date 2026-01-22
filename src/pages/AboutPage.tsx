aea4073dccf5e396615e8f3ebbc21e5175595b10
Dbrowne14 <davidbrowne1992@gmail.com>
2026-01-22 15:59:37 +0000

    adjusting animation of wave

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -12,12 +12,13 @@ export default function AboutPage() {
       md:grid-cols-2
       md:grid-rows-[4fr_4fr]
       md:[grid-template-areas:'AAA_BBB'_'CCC_CCC']
-      gap-4"
+      gap-4 "
+      
     >
-      <div className="[grid-area:AAA] justify-self-center self-center">
+      <div className="[grid-area:AAA] w-full h-full after:block after:border-b after:border-amberHome/20 after:w-5/6 after:mx-auto after:mt-6">
         <img
           src={myProfileImg}
-          className=" rounded-full w-[clamp(220px,30vw,300px)] h-[clamp(220px,30vw,300px)] object-cover object-[50%_20%] border-2 border-amberHome"
+          className=" justify-self-center self-center rounded-full w-[clamp(220px,30vw,300px)] h-[clamp(220px,30vw,300px)] object-cover object-[50%_20%] border-2 border-amberHome/50"
         />
       </div>
       <div className="[grid-area:BBB]"></div>
