d4dbf8e8f8029b28fb4cb49e1c0f7be008fab751
Dbrowne14 <davidbrowne1992@gmail.com>
2026-01-28 14:31:10 +0000

    highlightcolor global theme added

--- src/pages/Outlet/HomePage.tsx
+++ src/pages/Outlet/HomePage.tsx
@@ -3,13 +3,13 @@ function HomePage() {
     <div className="flex flex-col h-full w-fit justify-self-center items-center justify-center -translate-y-10 p-4 ">
       <h1 className="main-heading">
         Hello I'm {""}
-        <span className="text-highlightRed dark:text-highlightBlue bg-[rgba(151,104,90,0.1)] px-2">
+        <span className="text-highlightColor bg-[rgba(151,104,90,0.1)] px-2">
           David
         </span>
       </h1>
       <h1 className="main-heading cursor typewriter-animation">
         I'm a full-stack developer from{" "}
-        <span className="text-highlightRed dark:text-highlightBlue bg-[rgba(151,104,90,0.1)] px-2">
+        <span className="text-highlightColor bg-[rgba(151,104,90,0.1)] px-2">
           London.
         </span>
       </h1>
