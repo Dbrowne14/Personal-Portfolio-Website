e9cac207a175fd3c146d4c876942c9947f595bba
Dbrowne14 <davidbrowne1992@gmail.com>
2026-02-04 16:58:58 +0000

    button styling

--- src/pages/Outlet/Contact.tsx
+++ src/pages/Outlet/Contact.tsx
@@ -57,7 +57,7 @@ const Contact = () => {
           </div>
           <button
             type="submit"
-            className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            className="py-3 px-5 text-sm font-medium text-center text-amberHome rounded-lg bg-gray-50 border border-gray-300  dark:border-gray-600 dark:bg-gray-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
           >
             Send message
           </button>
