17241b087337230b329a36621d9104bb71f20cfc
Dbrowne14 <davidbrowne1992@gmail.com>
2026-02-06 11:27:20 +0000

    ease-in animation for bullet lists

--- src/components/ui/bulletList.tsx
+++ src/components/ui/bulletList.tsx
@@ -6,12 +6,12 @@ type Bullet = LinkProp & {
 
 const bulletList = ({ content, link }: Bullet) => {
   return (
-    <li className="flex justify-start items-center flex-row gap-4 h-10 md:h-18 w-full text-variableText ">
+    <li className="opacity-0 animate-[slideIn_0.7s_ease-out_forwards] flex justify-start items-center flex-row gap-4 h-10 md:h-18 w-full text-variableText ">
       <div className=" rounded-full md:h-10 md:w-10 dark:border sm:h-8 sm:w-8 h-6 w-6 border-foreground flex justify-center items-center hover:pb-2">
         {" "}
         {link}
       </div>
-      <p className=" transform ease-in duration 300 px-2 lg:px-8 drop-shadow-xl/20 h-full w-full flex justify-start items-center bg-lightBg sm:bg-transparent dark:bg-[rgba(27,3,49,0.9)] rounded-2xl">
+      <p className=" px-2 lg:px-8 drop-shadow-xl/20 h-full w-full flex justify-start items-center bg-lightBg sm:bg-transparent dark:bg-[rgba(27,3,49,0.9)] rounded-2xl">
         {content}
       </p>
     </li>
