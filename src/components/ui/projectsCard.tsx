997a02f9c10fc4ed7f7fcc01117ff8c53e42afd9
Dbrowne14 <davidbrowne1992@gmail.com>
2026-02-24 15:17:42 +0000

    changes to the app

--- src/components/ui/projectsCard.tsx
+++ src/components/ui/projectsCard.tsx
@@ -4,24 +4,29 @@ import { projectsData } from "../../data/projectsData";
 
 const projectsCard = () => {
   return projectsData.map((project) => (
-    <div className="flex flex-col rounded-2xl dark:bg-[rgba(27,3,49,0.9)] shadow-[0_6px_15px_2px_var(--color-highlightColor)] overflow-hidden transform-[perspective(1000px)_rotateX(0deg)_rotateY(0deg)_scale3d(1,1,1)]
-transition-transform duration-300 ease-out
-hover:transform-[perspective(1200px)_rotateX(10deg)_rotateY(15deg)_scale3d(1.05,1.05,1.05)]">
-
-      <div className="h-1/2 w-full">
-        <img className="w-full h-full object-cover border-b-3 border-highlightColor" src={project.imageLink} />
-      </div>
-
-      <div className="flex flex-col flex-1 p-4">
-        <h1 className="mb-1 text-variableText font-bold text-highlightColor">
-          {project.title}
-        </h1>
-        <p className="text-variableText">{project.description}</p>
-        <div className="flex flex-wrap mt-4 gap-2">
-          <TechUsed languages={project.languages} />
+    <a href={project.externalLink} key={project.title} target="_blank" className="block">
+      <div
+        className="flex flex-col rounded-2xl dark:bg-[rgba(27,3,49,0.9)] shadow-[0_6px_15px_2px_var(--color-highlightColor)] overflow-hidden transform-[perspective(1000px)_rotateX(0deg)_rotateY(0deg)_scale3d(1,1,1)]
+      transition-transform duration-300 ease-out
+      hover:transform-[perspective(1200px)_rotateX(10deg)_rotateY(15deg)_scale3d(1.05,1.05,1.05)]"
+      >
+        <div className="h-1/2 w-full">
+          <img
+            className="w-full h-full object-cover border-b-3 border-highlightColor"
+            src={project.imageLink}
+          />
+        </div>
+        <div className="flex flex-col flex-1 p-4">
+          <h1 className="mb-1 text-variableText font-bold text-highlightColor">
+            {project.title}
+          </h1>
+          <p className="text-variableText">{project.description}</p>
+          <div className="flex flex-wrap mt-4 gap-2">
+            <TechUsed languages={project.languages} />
+          </div>
         </div>
       </div>
-    </div>
+    </a>
   ));
 };
 
--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,13 +1,14 @@
 import davidWebsite from "../assets/Image 03-02-2026 at 17.35.jpg";
-import spotifyApp from "../assets//portfolio_images/spotifyapp.jpg"
+import spotifyApp from "../assets//portfolio_images/spotifyapp.jpeg";
 import { AllowedLanguages } from "@/types/dataTypes";
-import redditAPI from "../assets/portfolio_images/Reddit_Api.jpeg"
+import redditAPI from "../assets/portfolio_images/Reddit_Api.jpeg";
 
 type ProjectsData = {
   title: string;
   imageLink: string;
   description: string;
   languages: AllowedLanguages[];
+  externalLink: string;
 };
 //add a link for clicking
 
@@ -26,6 +27,7 @@ export const projectsData: ProjectsData[] = [
       "ExpressJS",
       "Tailwind",
     ],
+    externalLink: "https://redcanvas.netlify.app/"
   },
   {
     title: "Spotify Playlist Game",
@@ -33,6 +35,7 @@ export const projectsData: ProjectsData[] = [
     description:
       "Interactive music game leveraging Spotify’s OAuth 2.0 authentication",
     languages: ["React", "TypeScript", "JavaScript", "Tailwind"],
+    externalLink: "https://davejams.netlify.app/",
   },
   {
     title: "Custom Game Tracker for MTG",
@@ -40,17 +43,20 @@ export const projectsData: ProjectsData[] = [
     description:
       "A tracker for the popular trading card game Magic the Gathering for it's Chaos Draft format",
     languages: ["React", "TypeScript", "JavaScript", "CSS", "HTML"],
+    externalLink: "",
   },
   {
     title: "Calendar Scheduler",
     imageLink: davidWebsite,
     description: "A calendar scheduler for memos and required inputs",
     languages: ["React", "JavaScript", "CSS", "HTML"],
+    externalLink: "",
   },
   {
     title: "Big Boss Machine",
     imageLink: davidWebsite,
     description: "A machine for big bosses ",
     languages: ["ExpressJS", "NodeJS"],
+    externalLink: "",
   },
 ];
